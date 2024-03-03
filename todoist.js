976b204b0ed02f7413cd9b010990be739235c749
Luis Enrique Larrea <enrique.larrea@alumno.udg.mx>
2024-03-03 13:23:39 -0600

    login todoist

--- todoist.js
+++ todoist.js
@@ -1,10 +1,11 @@
 const puppeteer = require('puppeteer');
+const props = require('./app_properties');
 
 (async () => {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({
     headless: false,
-    slowMo: 400
+    slowMo: 200
   });
   const page = await browser.newPage();
 
@@ -16,7 +17,7 @@ const puppeteer = require('puppeteer');
   await page.waitForSelector(taskSelector)
     .then(() => console.log('got it'));
 
-  const result = await page.evaluate(() => {
+  const tasks = await page.evaluate(() => {
     const nodeList = document.querySelectorAll("[data-list-id='55d39827b8629b45cb9c7231'] .NdQKKfeqJDDdX3")
     const tasks = [];
     nodeList.forEach(function(item){
@@ -25,7 +26,11 @@ const puppeteer = require('puppeteer');
     return tasks
   })
   
-  console.log(result)
+  await page.goto('https://app.todoist.com/auth/login');
+  await page.type('#element-0', props.email);
+  await page.type('#element-3', props.password);
+  await page.click("button[type=submit]");
+  await page.waitForNavigation();
   
   await browser.close();
 })();
